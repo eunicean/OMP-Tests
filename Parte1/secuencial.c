2f1839f63158f1820858b0c66cb3c2d8dc5e6d84
Eunice Mata <eunice.mata2992@gmail.com>
2025-08-13 00:37:11 -0600

    base

---
+++ Parte1/secuencial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char const *argv[])
+{
+    double start, end;
+    long long sum = 0;
+    int *arr = malloc(100000000 * sizeof(int));
+    return 0;
+}
