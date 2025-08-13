e236e41bdcc09d224ecc342ea346760126bb89f2
Eunice Mata <eunice.mata2992@gmail.com>
2025-08-13 00:44:45 -0600

    secuencial program

--- Parte1/secuencial.c
+++ Parte1/secuencial.c
@@ -2,10 +2,26 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int main(int argc, char const *argv[])
-{
+#define N 100000000
+
+int main() {
     double start, end;
     long long sum = 0;
-    int *arr = malloc(100000000 * sizeof(int));
+    int *giga = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        giga[i] = i;
+    }
+
+    start = omp_get_wtime();
+    for (int i = 0; i < N; i++) {
+        sum += giga[i];
+    }
+    end = omp_get_wtime();
+
+    printf("Suma: %lld\n", sum);
+    printf("Tiempo secuencial: %f segundos\n", end - start);
+
+    free(giga);
     return 0;
 }
